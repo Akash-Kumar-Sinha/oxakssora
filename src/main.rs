4dcefe1eff5e7ae58cb42e04943b9750c0f9363f
Akash-Kumar-Sinha <akashkumarsinha304@gmail.com>
2026-08-11 04:48:33 +0530

    restructuring

--- src/main.rs
+++ src/main.rs
@@ -1,52 +1,8 @@
-use std::sync::Arc;
+use winit::event_loop::EventLoop;
 
-use winit::{
-    application::ApplicationHandler,
-    dpi::{PhysicalPosition, PhysicalSize, Position, Size},
-    event::WindowEvent,
-    event_loop::{ActiveEventLoop, EventLoop},
-    window::WindowAttributes,
-};
+use crate::window::App;
 
-#[derive(Default)]
-struct App {
-    window: Option<Arc<winit::window::Window>>,
-}
-
-impl ApplicationHandler for App {
-    fn resumed(&mut self, event_loop: &ActiveEventLoop) {
-        let window_attributes: WindowAttributes = WindowAttributes::default()
-            .with_inner_size(Size::Physical(PhysicalSize::new(800, 600)))
-            .with_title("oxakssora".to_string())
-            .with_resizable(true)
-            .with_position(Position::Physical(PhysicalPosition::new(0, 0)));
-
-        let window = {
-            let window = ActiveEventLoop::create_window(event_loop, window_attributes).unwrap();
-            Arc::new(window)
-        };
-
-        self.window = Some(window)
-    }
-
-    fn window_event(
-        &mut self,
-        event_loop: &ActiveEventLoop,
-        _window_id: winit::window::WindowId,
-        event: winit::event::WindowEvent,
-    ) {
-        match event {
-            WindowEvent::CloseRequested => {
-                println!("The close button was pressed; stopping");
-                event_loop.exit();
-            }
-            WindowEvent::RedrawRequested => {
-                self.window.as_ref().unwrap().request_redraw();
-            }
-            _ => {}
-        }
-    }
-}
+mod window;
 
 fn main() {
     env_logger::init();
